60d72dfee7abe3d81d36b9fe131c7e70113ee35a
nanasnumber <nanasnumber@gmail.com>
2024-06-11 14:35:55 -0400

    Initial commit: move page-interact widget to standalone app for columod interactive demo

---
+++ main.js
@@ -0,0 +1,14 @@
+import '/lib/css/main.css';
+
+import '/lib/enlezlab-ui/components/hero-main.js';
+import '/lib/enlezlab-ui/components/media-object.js';
+import '/lib/enlezlab-ui/components/media-object-reverse.js';
+import '/lib/enlezlab-ui/components/site-header.js';
+import '/lib/enlezlab-ui/components/site-footer.js';
+
+import '/lib/widget/logo-columod.js';
+import '/lib/widget/image-input.js';
+import '/lib/widget/component-control.js';
+import '/lib/widget/page-interact.js';
+
+
